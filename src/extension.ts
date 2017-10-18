268b2140b3d9063322c46627232bdaed64a296b9
Daksh Miglani <hello@dak.sh>
2017-10-18 18:37:03 +0530

    basic workflow

--- src/extension.ts
+++ src/extension.ts
@@ -6,19 +6,21 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "hex-to-rgba" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.convertToRGBA', () => {
         // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        if(typeof vscode.window.activeTextEditor != 'undefined') {
+            let editor = vscode.window.activeTextEditor
+            if(editor.document.getText(editor.selection) !== '') {
+                
+            } else {
+                vscode.window.showErrorMessage('Select Something to convert to RGBA')
+            }
+        } else {
+            vscode.window.showErrorMessage('Please Open up a File to Edit.')
+        }
     });
 
     context.subscriptions.push(disposable);
